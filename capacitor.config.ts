c7cf9361d72b10ac3e8450d0b740b904b8e71e42
rentamac <rentamac@Mac-mini-de-rentamac.local>
2026-02-25 15:05:47 -0800

    Add privacy page

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,35 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nlvipteam.app',
-  appName: 'NL VIP TEAM',
+  appId: 'com.nlvipnutrition.app',
+  appName: 'NLVIP Nutrition',
   webDir: 'out',
   server: {
-    androidScheme: 'https'
-  },
-  ios: {
-    contentInset: 'automatic',
-    preferredContentMode: 'mobile',
-    scheme: 'NL VIP TEAM'
-  },
-  android: {
-    backgroundColor: '#030303'
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: '#030303',
-      androidSplashResourceName: 'splash',
-      androidScaleType: 'CENTER_CROP',
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true
-    },
-    StatusBar: {
-      style: 'DARK',
-      backgroundColor: '#030303'
-    }
+    url: 'https://app.nlvipnutrition.com',
+    cleartext: false
   }
 };
 
